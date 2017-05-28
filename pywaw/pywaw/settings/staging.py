77925d2c4d52af0d6e5fd997cf67c644b443ff70
Wojciech Bederski <github@wuub.net>
2017-05-28 21:08:41 +0200

    make ALLOWED_HOSTS great again (also stg/prod safety net)

--- pywaw/pywaw/settings/staging.py
+++ pywaw/pywaw/settings/staging.py
@@ -8,3 +8,5 @@ TEMPLATE_DEBUG = DEBUG
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../../media')
 
 DATABASES = {'default': dj_database_url.config()}
+
+ALLOWED_HOSTS += ['pylight-staging.herokuapp.com']
