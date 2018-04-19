75488ab052bc2db7222e4c44cd21ea9ffe528905
Agata Grdal <agatagrdal@gmail.com>
2018-04-19 17:05:41 +0200

    Show on main page only active sponsors and on meetup detail only meetup sponsors

--- pylight/misc/views.py
+++ pylight/misc/views.py
@@ -13,8 +13,7 @@ class HomeView(generic.TemplateView):
         except meetups_models.Meetup.DoesNotExist:
             context['upcoming_meetup'] = None
         context['partners'] = models.Partner.objects.filter(is_public=True)
-        context['sponsors'] = meetups_models.Sponsor.objects.all()
-        print(context)
+        context['sponsors'] = meetups_models.Sponsor.objects.filter(is_active=True)
         return context
 
 
